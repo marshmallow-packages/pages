57d619278db85536beb3058f1cea5069ce8a21f5
Stef van Esch <stef@marshmallow.dev>
2020-05-21 16:14:25 +0200

    Styling updates and issue fix

--- src/app/Console/Commands/LayoutCommand.php
+++ src/app/Console/Commands/LayoutCommand.php
@@ -42,80 +42,90 @@ class LayoutCommand extends Command
     {
         $this->prepareFileStructure();
 
-    	$this->title = $this->ask('Please provide a name for your layout');
+        $this->title = $this->ask('Please provide a name for your layout');
 
-    	$slug = str_slug($this->title);
-    	$upper_camel = ucfirst(Str::camel(str_slug($this->title)));
+        $slug = str_slug($this->title);
+        $upper_camel = ucfirst(Str::camel(str_slug($this->title)));
 
-    	$name_examle = $slug;
-    	$class_examle = (strpos($upper_camel, 'Layout') === false) ? $upper_camel . 'Layout' : $upper_camel;
-    	$component_example = (strpos($upper_camel, 'Component') === false) ? $upper_camel . 'Component' : $upper_camel;
-    	
+        $name_examle = $slug;
+        $class_examle = (strpos($upper_camel, 'Layout') === false)
+                                ? $upper_camel . 'Layout'
+                                : $upper_camel;
 
-    	$this->name = $this->ask('Please provide a slug for your layout', $name_examle);
-    	$this->layout_class = $this->ask('Please provide a class name for your layout', $class_examle);
-    	$this->component_class_path = $this->ask('Please enter a name for you component', '\App\View\Components\\' . $component_example . '::class');
-  		
-    	$this->component_class_name = $this->getComponentClassFromPath($this->component_class_path);
+        $component_example = (strpos($upper_camel, 'Component') === false)
+                                ? $upper_camel . 'Component'
+                                : $upper_camel;
 
-        
+
+        $this->name = $this->ask('Please provide a slug for your layout', $name_examle);
+        $this->layout_class = $this->ask('Please provide a class name for your layout', $class_examle);
+        $this->component_class_path = $this->ask('Please enter a name for you component', '\App\View\Components\\' . $component_example . '::class');
+
+        $this->component_class_name = $this->getComponentClassFromPath($this->component_class_path);
+
+
         $this->fields = $this->getStubContent('LayoutFields');
         $this->use = '';
 
         if ($this->option('empty')) {
             $this->fields = '';
-            $this->use = 'use \Marshmallow\Pages\Flexible\Layouts\Traits;';
+            $this->use = 'use \Marshmallow\Pages\Flexible\Layouts\Traits\EmptyLayout;';
         }
 
-  		file_put_contents(
-            app_path('Flexible/Layouts/'. $this->layout_class .'.php'), 
+        file_put_contents(
+            app_path('Flexible/Layouts/'. $this->layout_class .'.php'),
             $this->parseStubContent('Layout')
         );
 
         file_put_contents(
-    		app_path('View/Components/'. $this->component_class_name .'.php'), 
-    		$this->parseStubContent('Component')
-    	);
+            app_path('View/Components/'. $this->component_class_name .'.php'),
+            $this->parseStubContent('Component')
+        );
 
-    	file_put_contents(
-    		resource_path('views/components/'. $slug .'.blade.php'), 
-    		$this->parseStubContent('View')
-    	);
+        file_put_contents(
+            resource_path('views/components/'. $slug .'.blade.php'),
+            $this->parseStubContent('View')
+        );
     }
 
-    protected function getComponentClassFromPath ($path)
+    protected function getComponentClassFromPath($path)
     {
-    	$path = explode('::class', $path);
-    	$path = $path[0];
-    	$path = explode('\\', $path);
-    	return end($path);
+        $path = explode('::class', $path);
+        $path = $path[0];
+        $path = explode('\\', $path);
+        return end($path);
     }
 
-    protected function getParams ()
+    protected function getParams()
     {
-    	return [
-    		'{{component_class}}' => $this->component_class_name,
-    		'{{name}}' => $this->name,
+        return [
+            '{{component_class}}' => $this->component_class_name,
+            '{{name}}' => $this->name,
             '{{title}}' => $this->title,
             '{{class}}' => $this->layout_class,
             '{{component_class_path}}' => $this->component_class_path,
             '{{fields}}' => $this->fields,
             '{{use}}' => $this->use,
-    	];
+        ];
     }
 
-    protected function getStubContent ($stub_name)
+    protected function getStubContent($stub_name)
     {
         return file_get_contents(__dir__ . '/../../Stubs/'. $stub_name .'.stub');
     }
 
-    protected function parseStubContent ($stub_name)
+    protected function parseStubContent($stub_name)
     {
-    	$content = $this->getStubContent($stub_name);
-    	return strtr($content, $this->getParams());
+        $content = $this->getStubContent($stub_name);
+        return strtr($content, $this->getParams());
     }
 
-    protected function prepareFileStructure ()
+    /**
+     * Make sure all folders we need are available
+     *
+     * @return void
+     */
+    protected function prepareFileStructure()
     {
         $structure = [
             resource_path('views'),
--- src/app/Flexible/Layouts/Layout.php
+++ src/app/Flexible/Layouts/Layout.php
@@ -1,5 +1,16 @@
 <?php
 
+/**
+ * Layout
+ *
+ * PHP version 7.4
+ *
+ * @category Layouts
+ * @package  Marshmallow\Pages
+ * @author   Stef van Esch <stef@marshmallow.dev>
+ * @license  MIT Licence
+ * @link     https://marshmallow.dev
+ */
 namespace Marshmallow\Pages\Flexible\Layouts;
 
 use Laravel\Nova\Fields\Text;
@@ -7,11 +18,38 @@ use Laravel\Nova\Fields\Markdown;
 use Mdixon18\Fontawesome\Fontawesome;
 use Whitecube\NovaFlexibleContent\Layouts\Layout as BaseLayout;
 
+/**
+ * Collection of functions available to all layouts
+ *
+ * @category Layouts
+ * @package  Marshmallow\Pages
+ * @author   Stef van Esch <stef@marshmallow.dev>
+ * @license  MIT Licence
+ * @link     https://marshmallow.dev
+ */
 class Layout extends BaseLayout
 {
-	public function render ()
+    /**
+     * Render the view component for this layout.
+     *
+     * @return string HTML of this layout
+     */
+    public function render()
     {
-    	$component_class_name = $this->getComponentClass();
+        $component_class_name = $this->getComponentClass();
         return (new $component_class_name($this))->render();
+    }
+
+
+    /**
+     * Get the public url of an image
+     *
+     * @param string $field Field from the current layout where an image is stored.
+     *
+     * @return string Public url of an image
+     */
+    public function getImage(string $field)
+    {
+        return asset('storage/' . $this->{$field});
     }
 }
