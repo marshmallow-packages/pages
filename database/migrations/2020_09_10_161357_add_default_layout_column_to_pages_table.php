375fe8a5d5457643ba1cd03e37700bce182609ff
Stef van Esch <stef@marshmallow.dev>
2020-10-08 15:42:10 +0200

    Added a view column tot the pages table

---
+++ database/migrations/2020_09_10_161357_add_default_layout_column_to_pages_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddDefaultLayoutColumnToPagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('pages', function (Blueprint $table) {
+            $table->string('view')->after('layout')->default('Default')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('pages', function (Blueprint $table) {
+            $table->dropColumn('view');
+        });
+    }
+}
